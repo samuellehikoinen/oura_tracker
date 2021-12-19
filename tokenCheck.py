6da38103561d1dbaddd919d5857f86a51bca4142
samuellehikoinen <samuel.lehikoinen@gmail.com>
2021-12-19 21:33:32 +0200

    Removed excess imports

--- tokenCheck.py
+++ tokenCheck.py
@@ -2,7 +2,6 @@ import requests
 import os
 from main import API_URL
 from main import AUTH_TOKEN_DEFINITION
-from main import AUTH_TOKEN
 
 def checkToken():
     """Checks if an authentication token exists and puts it in file"""
